8330675efe908daf196be18d163d3448da32a0b4
466548 <tsuka.as@gmail.com>
2019-05-09 17:11:31 +0900

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
   screenX = 0;
   screenY = 0;
   pageX = 0;
@@ -17,7 +16,7 @@ export class AppComponent  {
   offsetY = 0;
 
   //領域内でマウスポインターを動かした時その座標を表示
-  show(e:any) {
+  show(e: any) {
     this.screenX = e.screenX;
     this.screenY = e.screenY;
     this.pageX = e.pageX;
