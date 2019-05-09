071976ec78b6d25317d4055ef45b6a6726ec9713
466548 <tsuka.as@gmail.com>
2019-05-09 16:57:13 +0900

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'mouse',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'Angular';
+  screenX = 0;
+  screenY = 0;
+  pageX = 0;
+  pageY = 0;
+  clientX = 0;
+  clientY = 0;
+  offsetX = 0;
+  offsetY = 0;
+
+  //領域内でマウスポインターを動かした時その座標を表示
+  show(e:any) {
+    this.screenX = e.screenX;
+    this.screenY = e.screenY;
+    this.pageX = e.pageX;
+    this.pageY = e.pageY;
+    this.clientX = e.clientX;
+    this.clientY = e.clientY;
+    this.offsetX = e.offsetX;
+    this.offsetY = e.offsetY;
+  }
+
 }
